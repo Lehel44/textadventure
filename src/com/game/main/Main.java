8dbbdb02e99802f1616a0499f3d6a10ddbc1c61e
Atamester <atamester@gmail.com>
2015-07-06 22:37:32 +0200

    Kijavítottam Máté faszságát

--- src/com/game/main/Main.java
+++ src/com/game/main/Main.java
@@ -21,7 +21,7 @@ public class Main {
 		Player enemy = new Player();
 		enemy.setAttack(23);
 		enemy.setHealth(100);
-		enemy.setName("Lehel : a noob szar enemy");
+		enemy.setName("Lehel : az istencsászár enemy");
 		
 		//Fight létrehozása
 		Fight fight = new Fight();
